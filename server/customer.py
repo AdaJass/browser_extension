1fc19b0ecf3b2d4c15b28d22f0c05ea2baa6b196
adajass <jass.ada@qq.com>
2017-08-28 19:56:47 +0800

    kdjg

--- server/customer.py
+++ server/customer.py
@@ -1,5 +1,11 @@
 #this class store the cumstomer's curent state
 import uuid
+import mongo as db
+
+__description_attrib=list(db.descriptAttr.find({}))
+description_attrib={}
+for it in __description_attrib:
+    description_attrib[it['_id']]=it.pop('_id')
 
 class Customer:
     def __init__(self, customerid, currentpage):
@@ -9,7 +15,9 @@ class Customer:
             self.customerid = customerid
 
         self.currentpage = currentpage
-
+        self.description = db.description.find_one({'_id': self.cumstomerid})
+        self.basic_info = None
+        self.page_contact = None
 
     def get_current_contact(self, allcus):
         '''
@@ -17,13 +25,36 @@ class Customer:
         allcus is a dict which storge all cumstomer lisk: {'custID': CustomerObject}
         '''
         page_con=[]
-        for i,it in enumerate(allcus):
-            if it.currentpage == self.currentpage:
-                page_con.append(i)
+        for cid,obj in enumerate(allcus):
+            if obj.currentpage == self.currentpage:
+                page_con.append(cid)
         self.page_contact = page_con
-        
+              
         pass
     
-    def get_history(self):
+    
+    def get_basic_info(self):
+        cursor=db.customer.find_one({'_id': self.customerid})
+        friend=cursor['friend']
+        friends=dict()
+        for f in friend:
+            k=f.split('_')   ##  !!!!!! customer id and tag name can't contain '_'            
+            if friends.get(k[-1]) == None:
+                friends[k[-1]]=k[0]
+            else:
+                friends[k[-1]].append(k[0])
+        self.basic_info=cursor
+        self.basic_info['friend']=friends
+        pass
+
+    def get_history(self, date):
+        his=db.history.find({'customerId':self.customerid, 'date':{'$gte':date}).sort('date')
+        his=list(his)
+        return his
+
+    def get_profile(self, start_date, end_date):
+        pro=db.profile.find({'customerId':self.customerid, 'date':{'$gte':date, '$lte': end_date}).sort('date')
+        pro=list(pro)
+        return pro    
         pass
 
--- server/mongo.py
+++ server/mongo.py
@@ -2,4 +2,38 @@ import pymongo
 from pymongo import MongoClient
 __client = MongoClient('mongodb://localhost:27017')
 
+customer=__client.extension.customer
+history=__client.extension.history
+profile=__client.extension.profile
+descriptAttr=__client.extension.descriptAttr
+description=__client.extension.description
+chat=__client.extension.chat
+problemSet=__client.extension.problemSet
+
+"""
+customer={nickName, _id, birthday, age, sex, location, birthPlace, signInDate, bloodType, signature(one word), qq, wechat, phone, email, friend:[customerId_tagName, customerId_tagName, ...]} #customere static information
+history={customerId, date, domain, url}
+profile={customerId, date, saying(usually several words represent the mood, will save with format that contain picture or emoj)}
+descriptAttr={_id, feature, example:[will list some of the customerid]}
+description={customerId, attrPairs:{attrId1:likeRate, attrId2: likeRate, ....}}
+chat={origin, destination, date, words}
+problemSet={problem, category ,solveUrl:[up to 5 url], contributor: [up to 3 customerId], buldDate, lastUpdate}  #search engine recommend
+"""
+
+
+if __name__ == '__main__':
+    from datetime import datetime as dt
+    #db.customer.insert({nickName:'test', customerid:'test', birthday:'test', age:'test', sex:'test', location:'test', birthPlace:'test', signInDate:'test',_id:'test', bloodType:'test', signature:'test', qq:'test', wechat:'test', phone:'test', email:'test', friend:['customerId_tagName', 'customerId_tagName']})
+    
+    # customer.insert({'nickNmae': 'father', '_id': '3', 'sex': False, 'age': 0, 'friend':['1_default']})
+    # history.insert({'customerId':'1', 'date':dt.today(),'domain':'fun.com','url':'www.fun.com/adk/83'})
+    # profile.insert({'customerId':'1', 'date':dt.today(), 'saying':'i love you'})
+    # descriptAttr.insert({'_id':'die','feature':'ksgasjkdgkajddkgjakdjkgj', 'example':['1']})
+    # description.insert({'_id':'1', 'attrPairs':{'die':0.9, 'out':0.2}})
+    # chat.insert({'origin':'1', 'destination':'2', 'date': dt.today(), 'words':'i love you'})
+    # problemSet.insert({'problem':'why flower so red?', 'category': [] ,solveUrl':['www.baidu.com/skg/skg.html','baidu.teiba.com/akjg/2354.html',None,None,None], 'contributor':['1','2','3'], 'buildDate':dt.today(), 'lastUpdate': dt.today()})
+    # cursor = customer.find({'_id': 'test'})
+    # print(cursor)
+    # friend=cursor[0]['friend']
+    # print(friend)
 
