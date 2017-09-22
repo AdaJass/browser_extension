37ac56bcc7623dce670c5f3512d82c24a65cf11d
Jass Ada <Jass Ada>
2017-09-22 19:29:05 +0800

    dkjg

--- client/js/bg_2_content.js
+++ client/js/bg_2_content.js
@@ -1,13 +1,14 @@
-var bg_port = None;
+var bg_port = null;
 chrome.runtime.onConnect.addListener(function(port) {
     console.assert(port.name == "currentpage");
     bg_port = port;
     port.onMessage.addListener(function(msg) {
       if (msg.current_page)
-        port.postMessage(Contact_List);      
-    });
+        port.postMessage(Contact_List);  
+        //alert('back recieve msg!');    
+    });    
 });
 
-if(bg_port){
-    bg_port.postMessage({barrager:{}});
-}
+setInterval(function(){if(bg_port){bg_port.postMessage({barrager:{}});}}, 20000);
+
+
--- client/js/content.js
+++ client/js/content.js
@@ -1,25 +1,26 @@
 var port = chrome.runtime.connect({name: "currentpage"});
+port.postMessage({current_page: "Knock knock"});
 var last_pagecontact=[]
-setInterval(function(){port.postMessage({current_page: 1});}, 1500);
+// setInterval(function(){port.postMessage({current_page: 1});}, 1500);
 
 port.onMessage.addListener(function(msg) {
     var pagecontact = msg.pagecontact
     if(pagecontact && (pagecontact.toString() != last_pagecontact.toString())){
         //here reinsert to page. 
-        last_pagecontact = pagecontact;       
+        last_pagecontact = pagecontact;             
     }
-    if (msg.barrager){
-        
+    if (msg.barrager){        
+        alert('front receive msg.');        
     }
     
     if (msg.login){
-
+        alert(9);
     }
     if (msg.chatWindow){
-        
+        alert(8);
     }
     if (msg.chatting){
-
+        alert(7);
     }    
 });
 
