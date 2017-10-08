34dcefd723d7f4e19047d53c9da9b348090232f8
adajass <jass.ada@qq.com>
2017-10-09 04:05:29 +0800

    gkaj

---
+++ client/js/chat_box_html.js
@@ -0,0 +1,86 @@
+var html_text =
+`<div class="chatBox">            
+   <div class="chatLeft">
+       <div class="chat01">
+           <div class="chat01_title">
+               <ul class="talkTo">
+                   <li><a href="javascript:;">王旭</a></li>
+               </ul>
+               <a class="close_btn" href="javascript:;"></a>
+           </div>
+           <div class="chat01_content">
+               <div class="message_box mes1">
+               </div>
+               <div class="message_box mes2">
+               </div>
+               <div class="message_box mes3" style="display: block;">
+               </div>
+               <div class="message_box mes4">
+               </div>
+               <div class="message_box mes5">
+               </div>
+               <div class="message_box mes6">
+               </div>
+               <div class="message_box mes7">
+               </div>
+               <div class="message_box mes8">
+               </div>
+               <div class="message_box mes9">
+               </div>
+               <div class="message_box mes10">
+               </div>
+           </div>
+       </div>
+       <div class="chat02">
+           <div class="chat02_title">
+               <a class="chat02_title_btn ctb01" href="javascript:;"></a>
+               <a class="chat02_title_btn ctb02" href="javascript:;"
+                   title="选择文件">                   
+               </a>
+               <a class="chat02_title_btn ctb03" href="javascript:;" title="选择附件">                   
+               </a>
+               <label class="chat02_title_t">
+               <a href="chat.htm" target="_blank">聊天记录</a></label>
+               <div class="wl_faces_box">
+                   <div class="wl_faces_content">
+                       <div class="title">
+                           <ul>
+                               <li class="title_name">常用表情</li>
+                               <li class="wl_faces_close"><span>&nbsp;</span></li>
+                           </ul>
+                       </div>
+                       <div class="wl_faces_main">
+                           <ul>                               
+                           </ul>
+                       </div>
+                   </div>
+                   <div class="wlf_icon">
+                   </div>
+               </div>
+           </div>
+           <div class="chat02_content">
+               <div contenteditable="true" id="textarea"></div>
+           </div>
+           <div class="chat02_bar">
+               <ul>
+                   <li style="left: 20px; top: 10px; padding-left: 30px;">ads here<a href="http://www.jq22.com/" target="_blank">Ads here</a></li>
+                   <li style="right: 5px; top: 5px;"><a href="javascript:;">
+                   <img></a></li>
+               </ul>
+           </div>
+       </div>
+   </div>
+   <div class="chatRight">
+       <div class="chat03">
+           <div class="chat03_title">
+               <label class="chat03_title_t">
+               成员列表</label>
+           </div>
+           <div class="chat03_content">
+               <ul>                                                
+               </ul>
+           </div>
+       </div>
+   </div>
+   <div style="clear: both;"></div>
+</div>`;
